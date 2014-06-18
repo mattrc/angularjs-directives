486cb037b113bf1fc70f50fce66fa10a40a647a9
Matias Alibertti <matias.alibertti@hinch.as>
2014-06-18 12:33:13 -0300

    Initial commit

---
+++ ngSwitch/js/app.js
@@ -0,0 +1,90 @@
+(function () {
+'use strict';
+
+var app = angular.module('app', []);
+
+/**
+ * Controller: Main
+ */
+app.controller('MainCtrl', function ($scope) {
+    $scope.sport = '';
+});
+
+
+/**
+ * Directive: mc-switch
+ */
+app.directive('mcSwitch', function () {
+    return {
+        controller: function () {
+            // Linker functions object
+            this.cases = {};
+        },
+        link: function (scope, el, attrs, ctrl) {
+
+            var caseElement,
+                caseScope;
+
+            // Watch for changes in "sport"
+            scope.$watch(attrs.mcSwitch, function (nv) {
+
+                // Linker function
+                var caseLinker = ctrl.cases['!' + nv] || ctrl.cases['?'];
+
+                // If found previos element
+                if (caseElement) {
+                    // Destroy scope
+                    caseScope.$destroy();
+                    // Remove element
+                    caseElement.remove();
+                    // Clear vars
+                    caseElement = caseScope = null;
+                }
+
+                // If the linker function exists
+                if (caseLinker) {
+                    // Create new scope
+                    caseScope = scope.$new();
+                    // Bind scope to linked element
+                    caseLinker(caseScope, function (element) {
+                        // Store linked element reference
+                        caseElement = element;
+                        // Append linked element
+                        el.append(caseElement);
+                    });
+                }
+
+            });
+        }
+    };
+});
+
+
+/**
+ * Directive: mc-switch-when
+ */
+app.directive('mcSwitchWhen', function () {
+    return {
+        transclude: 'element',
+        require: '^mcSwitch',
+        link: function (scope, el, attrs, mcSwitchCtrl, linker) {
+            mcSwitchCtrl.cases['!' + attrs.mcSwitchWhen] = linker;
+        }
+    };
+});
+
+
+/**
+ * Directive: mc-switch-default
+ */
+app.directive('mcSwitchDefault', function () {
+    return {
+        transclude: 'element',
+        require: '^mcSwitch',
+        link: function (scope, el, attrs, mcSwitchCtrl, linker) {
+            mcSwitchCtrl.cases['?'] = linker;
+        }
+    };
+});
+
+})();
---
+++ promises/js/services.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('Services', [])
+
+    // .config()
+
+    .service('user', function () {
+
+        return {
+            name: 'Matias',
+            email: 'info@matiasmancini.com.ar'
+        };
+
+    })
+
+    .service('TravelService', function ($q, $timeout) {
+
+        return {
+
+            getDeparture: function (user) {
+                var def = $q.defer();
+
+                $timeout(function () {
+                    def.resolve({
+                        userId: user.id,
+                        flightId: 'UA_343223',
+                        date: '01/14/2014 8:00 AM'
+                    });
+                }, (Math.random() * 2000));
+
+                return def.promise;
+            },
+
+            getFlight: function (flightId) {
+                var def = $q.defer();
+
+                $timeout(function () {
+                    def.resolve({
+                        id: flightId,
+                        pilot: 'Captain Morgan',
+                        plane: {
+                            make : 'Boeing 747 RC',
+                            model : 'TA-889'
+                        },
+                        status: 'onTime'
+                    });
+                }, (Math.random() * 2000));
+
+                return def.promise;
+            }
+
+        };
+
+    })
+
+    .service('WeatherService', function ($q, $timeout) {
+
+        return {
+            getForecast: function (date) {
+                var def = $q.defer();
+
+                $timeout(function () {
+                    def.resolve({
+                        date: date,
+                        forecast: 'rain'
+                    });
+                }, (Math.random() * 2000));
+
+                return def.promise;
+            }
+        };
+
+    });
