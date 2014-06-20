a45bb7a28b16ef794651b57b8d7129de666101d7
Matias Mancini <info@matiasmancini.com.ar>
2014-06-20 11:35:32 -0300

    Add inception files

---
+++ inception/js/app.js
@@ -0,0 +1,33 @@
+(function () {
+'use strict';
+
+angular.module('app', [])
+
+.directive('logCompile', function ($rootScope) {
+
+	$rootScope.log = '';
+
+	return {
+		controller: function ($scope, $attrs) {
+			$rootScope.log = $rootScope.log + ($attrs.logCompile + ' (controller)\n');
+		},
+		compile: function (tElement, tAttrs) {
+			$rootScope.log = $rootScope.log + (tAttrs.logCompile + ' (compile)\n');
+
+			// Prepend content
+			tElement.prepend(tAttrs.logCompile)
+
+			return {
+				pre: function ($scope, $element, $attrs) {
+					$rootScope.log = $rootScope.log + ($attrs.logCompile + ' (pre-link)\n');
+				},
+				post: function ($scope, $element, $attrs) {
+					$rootScope.log = $rootScope.log + ($attrs.logCompile + ' (post-link)\n');
+				}
+			}
+		}
+	}
+
+});
+
+})();
