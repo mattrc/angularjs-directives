6cd35a54502b2109765337f8391c98df6f0977ef
Matias Mancini <info@matiasmancini.com.ar>
2014-06-28 20:09:46 -0300

    templateUrl example

---
+++ templateUrl/js/app.js
@@ -0,0 +1,17 @@
+(function () {
+'use strict';
+
+angular.module('app', [])
+
+.directive('hsDirective', function () {
+	return {
+		restrict: 'E',
+		templateUrl: function (element, attr) {
+			var tpl = attr.hsTpl || 'paragraph';
+			return 'partials/' + tpl + '.html';
+		}
+	}
+})
+
+})();
+
