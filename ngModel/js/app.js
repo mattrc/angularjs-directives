feecd319466a38ec527350c27a2dc58c29411527
Matias Mancini <info@matiasmancini.com.ar>
2014-06-30 14:01:56 -0300

    Update ngModel

--- ngModel/js/app.js
+++ ngModel/js/app.js
@@ -39,45 +39,25 @@ angular.module('app', [])
 			};
 
 			ngModel.$render = function () {
-				window.log( '_---$render()_' );
-				window.log( 'ngModel.$modelValue: [c="color: MediumSeaGreen"]' + ngModel.$modelValue + '[c]' );
-				window.log( 'ngModel.$viewValue: [c="color: MediumSeaGreen"]' + ngModel.$viewValue + '[c]' );
-				window.log( 'scope.selected: [c="color: MediumSeaGreen"]' + scope.selected + '[c]\n' );
 				setSelected(ngModel.$viewValue || 'Not sure');
 			};
 
 			ngModel.$formatters.push(function (value) {
-				window.log( '_---$formatters(' + value + ')_' );
 				if (typeof value === 'string') {
-					window.log( 'returns: [c="color: DarkOrchid"]' + value.toUpperCase() + '[c]\n' );
 					return value.toUpperCase();
 				} else {
-					window.log( 'returns: [c="color: DarkOrchid"]' + value + '[c]\n' );
 					return value;
 				}
 			});
 
 			buttons.on('click', function (event) {
-				window.log( '_---onClick()_' );
-				window.log( 'ngModel.$modelValue: [c="color: OrangeRed"]' + ngModel.$modelValue + '[c]' );
-				window.log( 'ngModel.$viewValue: [c="color: OrangeRed"]' + ngModel.$viewValue + '[c]' );
-				window.log( 'scope.selected: [c="color: OrangeRed"]' + scope.selected + '[c]\n' );
-
-
 
 				scope.$apply(function () {
-					window.log('_---$setViewValue()_');
 					ngModel.$setViewValue(event.target.innerText);
-
-					// window.log('_scope.selected = value_');
-					// scope.selected = event.target.innerText;
 				});
 
 				setSelected(ngModel.$viewValue);
 
-				window.log( 'ngModel.$modelValue: [c="color: DodgerBlue"]' + ngModel.$modelValue + '[c]' );
-				window.log( 'ngModel.$viewValue: [c="color: DodgerBlue"]' + ngModel.$viewValue + '[c]' );
-				window.log( 'scope.selected: [c="color: DodgerBlue"]' + scope.selected + '[c]\n' );
 			});
 
 		},
