be8bc72e494beb8a01638e12207a21750ea21f9e
Matias Mancini <info@matiasmancini.com.ar>
2014-07-06 20:27:04 -0300

    Cleanup compile example

--- compile/js/app.js
+++ compile/js/app.js
@@ -3,34 +3,30 @@
 
 	var app = angular.module('app', []);
 
-	// Controller: AppController
 	app.controller('AppController', function ($scope) {
 		$scope.friends = buildFriends(1000);
 	});
 
-	// Directive: hsFriend
 	app.directive('hsFriend', function ($compile) {
 
 		return {
-			restrict: 'EA',
 			templateUrl: 'partials/template.html',
 			compile: function (element) {
 
-				// Extract the delete confirmation element
-				var confirm = element.find('.delete').remove();
+				// Extract the .confirm element
+				var confirm = element.find('.confirm').remove();
 
-				// We need to compile it separeately so that it can be
-				// transcluded an linked separately.
-				var transcludeConfirm = $compile(confirm);
+				// Compile it separeately for cloning later
+				var linker = $compile(confirm);
 
 				// Link function
 				return function (scope, element) {
 
-					// Inject the delete confirmation when the user clicks, just one time.
+					// insert the confirm element when the user clicks, just one time.
 					element.one('mousedown', 'button', function () {
 
-						// Transclude and link the DOM tree for the delete confirmation.
-						transcludeConfirm(scope, function (clone) {
+						// Link & clone
+						linker(scope, function (clone) {
 							// Append to element
 							element.append(clone);
 						});
@@ -40,19 +36,18 @@
 
 					});
 
-					scope.showConfirmation = function () {
-						scope.friend.deleting = true;
-					};
-
-					scope.hideConfirmation = function () {
-						scope.friend.deleting = false;
-					};
-
 				};
 			}
 		};
 
 	});
+
+
+
+
+
+
+
 
 
 
